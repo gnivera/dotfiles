44f22765a9b2d2f063fad5ee501337ebc80a11e4
Yuki Sato <yukisato.d@gmail.com>
2020-01-02 10:58:49 +0900

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,11 +14,12 @@ fi
 
 curl -o $HOME/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
-cd "$DOTPATH"
+cd "$DOTFILESPATH"
 
 for f in .??*; do
   [ "$f" = ".DS_Store" ] && continue
   [ "$f" = ".git" ] && continue
+  [ "$f" = ".gitignore" ] && continue
   ln -snfv "$HOME"/dotfiles/"$f" "$HOME"/"$f"
 done
 
