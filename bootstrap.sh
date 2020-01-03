3453942657c6e940c3da8aa874b229f2b67b0734
Yuki Sato <yukisato.d@gmail.com>
2020-01-03 09:40:44 +0900

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,6 +29,6 @@ if !(type brew > /dev/null 2>&1); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew bundle --global
+brew bundle -v --global
 
 cd "$CURRENTPATH" || exit
